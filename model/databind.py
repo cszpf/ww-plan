23826228d3d0f72d2646418ec6adea7cd7bc1145
cszpf <2900129289@qq.com>
2018-08-16 11:59:32 +0800

    updated databind interface

--- model/databind.py
+++ model/databind.py
@@ -5,6 +5,10 @@ import pandas as pd
 import connectTool
 cwd = os.getcwd()
 
+def template(items, ids):
+	return {'name':items[1],'id':items[0],'type':ids}
+
+
 class Connect(connectTool.connect):
     
     # 查询数据库
@@ -23,37 +27,37 @@ class Databind:
     def ADMIN_REGION_CODE(self, data):
         sql = 'SELECT region_code, region_name FROM micro_region WHERE LEVEL=1;'
         result = self.connect.query(self.connect.fenqi, sql)
-        return {_[0]:_[1] for _ in result}
+        return {'ADMIN_REGION_CODE':[template(_,'ADMIN_REGION_CODE') for _ in result]}
 
     def MICRO_REGION_CODE(self, data):
         sql = "SELECT region_code, region_name FROM micro_region WHERE LEVEL=2 AND parent_region_code='{}';"
         result = self.connect.query(self.connect.fenqi, sql.format(data['opt']['ADMIN_REGION_CODE']))
-        return {_[0]:_[1] for _ in result}
+        return {'MICRO_REGION_CODE':[template(_,'MICRO_REGION_CODE') for _ in result]}
 
     def MERCHANT_TYPE(self, data):
         sql = "SELECT DISTINCT merchant_type, merchant_type_name FROM merchant_industry;"
         result = self.connect.query(self.connect.fenqi, sql)
-        return {_[0]:_[1] for _ in result}
+        return {'MERCHANT_TYPE':[template(_,'MERCHANT_TYPE') for _ in result]}
 
     def SALE_NAME(self, data):
         sql = "SELECT DISTINCT sale_name FROM subbranch WHERE sale_name IS NOT NULL;"
         result = self.connect.query(self.connect.fenqi, sql)
-        return {str(i):_[0] for i, _ in enumerate(result)}
+        return {'SALE_NAME':[template([i,_[0]],'SALE_NAME') for i, _ in enumerate(result)]}
 
     def OPERATOR_NAME(self, data):
         sql = "SELECT DISTINCT operator_name FROM subbranch WHERE sale_name IS NOT NULL;"
         result = self.connect.query(self.connect.fenqi, sql)
-        return {str(i):_[0] for i, _ in enumerate(result)}
+        return {'OPERATOR_NAME':[template([i,_[0]],'OPERATOR_NAME') for i, _ in enumerate(result)]}
 
     def MERCHANT_ID(self, data):
         sql = "SELECT merchant_id, merchant_name FROM merchant;"
         result = self.connect.query(self.connect.fenqi, sql)
-        return {_[0]:_[1] for _ in result}
+        return {'MERCHANT_ID':[template(_,'MERCHANT_ID') for _ in result]}
 
     def SUBBRANCH_ID(self, data):
         sql = "SELECT subbranch_id, subbranch_name FROM subbranch WHERE merchant_id='{}';"
         result = self.connect.query(self.connect.fenqi, sql.format(data['opt']['MERCHANT']))
-        return {_[0]:_[1] for _ in result}
+        return {'SUBBRANCH_ID':[template(_,'SUBBRANCH_ID') for _ in result]}
 
 if __name__ == '__main__':
     databind = Databind()
